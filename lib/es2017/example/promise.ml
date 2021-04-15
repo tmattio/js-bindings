49771f546521ba50383e4afd08e965f89fca27e6
Thibaut <thibaut.mattio@gmail.com>
2021-04-15 11:39:41 +0200

    Move examples in subdirs

---
+++ lib/es2017/example/promise.ml
@@ -0,0 +1,9 @@
+open Es2017
+
+let (_ : unit Promise.t) =
+  let promise = Promise.create (fun ~resolve ~reject:_ -> resolve 1) in
+  Promise.then_
+    promise
+    ~onfulfilled:(fun value ->
+      Promise.resolve @@ print_endline (Printf.sprintf "Got %i" value))
+    ()
