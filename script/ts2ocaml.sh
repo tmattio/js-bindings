61d15d21517ca213a4b2992b5017107cad12526d
Thibaut <thibaut.mattio@gmail.com>
2021-04-15 11:35:19 +0200

    lib/ -> intf/

--- script/ts2ocaml.sh
+++ script/ts2ocaml.sh
@@ -15,7 +15,7 @@ for dir in $dirs; do
 
    for f in $files; do
     gen_f=$(basename "$f" | sed -e "s/\.d\.ts/\.mli/g" | sed -e "s/\//_/g" | sed -e "s/\./_/g" | sed -e "s/lib_//g" | sed -e "s/_mli/\.mli/g")
-    echo "node ts2ocaml.js $cwd/$dir/$f > $cwd/$dir/../lib/$gen_f"
+    echo "node ts2ocaml.js $cwd/$dir/$f > $cwd/$dir/../intf/$gen_f"
     node ts2ocaml.js $cwd/$dir/$f > $cwd/$dir/../gen/$gen_f
    done
 
